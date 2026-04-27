54aae4b0bcb970f2364ff2a136659c0be9ffb037
Nate Williams <natesclaw16@gmail.com>
2026-04-27 16:50:53 -0700

    Initial Commit

---
+++ Gospel Outreach Video/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gospel Outreach Video
+//
+//  Created by Nate Williams on 4/27/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gospel Outreach Video/Gospel_Outreach_VideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gospel_Outreach_VideoApp.swift
+//  Gospel Outreach Video
+//
+//  Created by Nate Williams on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct Gospel_Outreach_VideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
